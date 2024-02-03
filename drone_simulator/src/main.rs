6c268e3967753aaea581b95c151c02378d8e94ed
Andres <andresgarciar96@gmail.com>
2024-02-03 14:35:01 +0100

    base simulator implremented, drones move randomly for now

--- drone_simulator/src/main.rs
+++ drone_simulator/src/main.rs
@@ -2,7 +2,7 @@ use ggez::{conf, event, graphics, timer, Context, GameResult, mint};
 use rand::Rng;
 use nalgebra as na;
 
-const GRID_SIZE: f32 = 20.0;
+const GRID_SIZE: f32 = 500.0;
 const DRONE_RADIUS: f32 = 10.0;
 const NUM_DRONES: usize = 10;
 
@@ -20,19 +20,21 @@ impl Drone {
         }
     }
 
-    fn update(&mut self, ctx: &Context) {
+    fn update(&mut self, ctx: &Context) -> GameResult<()>{
         let mut rng = rand::thread_rng();
 
         let delta_time = timer::delta(ctx).as_secs_f32();
         let displacement = na::Vector2::new(
-            rng.gen_range(-1.0..1.0),
-            rng.gen_range(-1.0..1.0),
+            rng.gen_range(-5.0..10.0),
+            rng.gen_range(-5.0..10.0),
         ) * delta_time;
 
         self.position = mint::Point2 {
             x: self.position.x + displacement.x,
             y: self.position.y + displacement.y,
         };
+
+        Ok(())
     }
 
     fn draw(&self, ctx: &mut Context) -> GameResult<()> {
@@ -48,6 +50,44 @@ impl Drone {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+struct MainState {
+    drones: Vec<Drone>,
+}
+
+impl MainState {
+    fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let drones: Vec<Drone> = (0..NUM_DRONES)
+            .map(|_| Drone::new(rng.gen_range(0.0..GRID_SIZE), rng.gen_range(0.0..GRID_SIZE)))
+            .collect();
+        MainState{drones}
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        for drone in &mut self.drones {
+            drone.update(ctx)?
+        }
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::Color::BLACK);
+
+        for drone in &self.drones {
+            drone.draw(ctx)?;
+        }
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("drone_simulator", "your_username")
+        .window_setup(conf::WindowSetup::default().title("Drone Simulator"))
+        .window_mode(conf::WindowMode::default().dimensions(GRID_SIZE, GRID_SIZE).resizable(true));
+
+    let (ctx, event_loop) = cb.build()?;
+    let state = MainState::new();
+    event::run(ctx, event_loop, state)
 }
